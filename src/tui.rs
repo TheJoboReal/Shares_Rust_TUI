a3c2a6fc30b18bb1e16efd5503e93e20fdfb70c6
Kasper Bloch Mathiasern <kasper.jobo@gmail.com>
2025-06-27 11:30:17 +0200

    Added handle_key function

--- src/tui.rs
+++ src/tui.rs
@@ -1,7 +1,7 @@
 use color_eyre::eyre::{Ok, Result};
 use ratatui::{
     DefaultTerminal, Frame,
-    crossterm::event::{self, Event},
+    crossterm::event::{self, Event, KeyEvent},
     layout::{Constraint, Layout},
     style::{Color, Style, Stylize},
     widgets::{Block, List, ListItem, ListState, Paragraph, Widget},
@@ -13,6 +13,7 @@ use crate::share_calc::Person;
 pub struct AppState {
     pub items: Vec<Person>,
     pub list_state: ListState,
+    pub is_add_new: bool,
 }
 
 pub fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
@@ -20,25 +21,38 @@ pub fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()
         // Rendering
         terminal.draw(|f| render(f, app_state))?;
 
-        // Input handling
         if let Event::Key(key) = event::read()? {
-            match key.code {
-                event::KeyCode::Esc => {
-                    break;
-                }
-                event::KeyCode::Char('k') => {
-                    app_state.list_state.select_previous();
-                }
-                event::KeyCode::Char('j') => {
-                    app_state.list_state.select_next();
-                }
-                _ => {
-                    // Handle other keys
-                }
+            if handle_key(key, app_state) {
+                break;
             }
         }
+
+        // Input handling
     }
     Ok(())
+}
+
+fn handle_key(key: KeyEvent, app_state: &mut AppState) -> bool {
+    match key.code {
+        event::KeyCode::Esc => {
+            return true;
+        }
+        event::KeyCode::Char('k') => {
+            app_state.list_state.select_previous();
+        }
+        event::KeyCode::Char('j') => {
+            app_state.list_state.select_next();
+        }
+        event::KeyCode::Char('D') => {
+            if let Some(index) = app_state.list_state.selected() {
+                app_state.items.remove(index);
+            }
+        }
+        _ => {
+            // Handle other keys
+        }
+    }
+    false
 }
 
 pub fn render(frame: &mut Frame, app_state: &mut AppState) {
