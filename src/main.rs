6aef7d5e10d8edfeadb9f1323948d1d14bf1adc8
Kasper Bloch Mathiasern <kasper.jobo@gmail.com>
2025-06-27 10:29:07 +0200

    we have a border

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod share_calc;
 mod tui;
+use tui::AppState;
 use tui::run;
 
 fn main() -> Result<(), color_eyre::Report> {
+    let mut state = AppState::default();
     color_eyre::install()?;
 
     let terminal = ratatui::init();
-    let result = run(terminal);
+    let result = run(terminal, &mut state);
 
     ratatui::restore();
     result
--- src/tui.rs
+++ src/tui.rs
@@ -1,21 +1,26 @@
 use color_eyre::eyre::{Ok, Result};
 use ratatui::{
     DefaultTerminal, Frame,
-    crossterm::event::{self, Event},
-    widgets::{Paragraph, Widget},
+    crossterm::{
+        event::{self, Event},
+        style::Color,
+    },
+    layout::{Constraint, Layout},
+    style::Stylize,
+    widgets::{Block, Paragraph, Widget},
 };
 
 use crate::share_calc::Person;
 
 #[derive(Debug, Default)]
-struct AppState {
+pub struct AppState {
     items: Vec<Person>,
 }
 
-pub fn run(mut terminal: DefaultTerminal) -> Result<()> {
+pub fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     loop {
         // Rendering
-        terminal.draw(render)?;
+        terminal.draw(|f| render(f, app_state))?;
 
         // Input handling
         if let Event::Key(key) = event::read()? {
@@ -32,6 +37,15 @@ pub fn run(mut terminal: DefaultTerminal) -> Result<()> {
     Ok(())
 }
 
-pub fn render(frame: &mut Frame) {
+pub fn render(frame: &mut Frame, app_state: &mut AppState) {
+    let [border_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(frame.area());
+
+    Block::bordered()
+        .border_type(ratatui::widgets::BorderType::Rounded)
+        .fg(Color::Yellow)
+        .render(border_area, frame.buffer_mut());
+
     Paragraph::new("Hello World!").render(frame.area(), frame.buffer_mut());
 }
